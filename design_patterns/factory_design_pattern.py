0c3087ae5e2544947ea5f81ac79af8aa5261d858
fuseMuskan <141207855+fuseMuskan@users.noreply.github.com>
2023-08-09 13:47:56 +0545

    refactor: factory_design_pattern.py file

--- design_patterns/factory_design_pattern.py
+++ design_patterns/factory_design_pattern.py
@@ -2,26 +2,26 @@ import logging
 import datetime
 
 class FileLogger:
-    def __init__(self) -> None:
+    def __init__(self):
         print("File Logging")
 
     def logging(self, msg):
-        return logging.info(f"File Logging {msg} at {datetime.datetime.now()}")
+        logging.info(f"File Logging {msg} at {datetime.datetime.now()}")
 
 class ConsoleLogger:
-    def __init__(self) -> None:
+    def __init__(self):
         print("Console Logging")
 
     def logging(self, msg):
-        return logging.warning(f"Console Logging {msg} at {datetime.datetime.now()}")
+        logging.warning(f"Console Logging {msg} at {datetime.datetime.now()}")
 
 class DatabaseLogger:
-    def __init__(self) -> None:
+    def __init__(self):
         print("Database Logging")
 
     def logging(self, msg):
-        return logging.info(f"Database Logging {msg} at {datetime.datetime.now()}")
-    
+        logging.info(f"Database Logging {msg} at {datetime.datetime.now()}")
+
 class LoggerFactory:
     @staticmethod
     def create_logger(logger_type: str):
@@ -33,14 +33,17 @@ class LoggerFactory:
         }
         return loggers[logger_type]()
 
+def main():
+    logging.basicConfig(level=logging.INFO)
 
-logging.basicConfig(level=logging.INFO)
+    message = "secret"
+    file = LoggerFactory.create_logger("File")
+    console = LoggerFactory.create_logger("Console")
+    database = LoggerFactory.create_logger("Database")
 
-message = "secret"
-file = LoggerFactory.create_logger("File")
-console = LoggerFactory.create_logger("Console")
-database = LoggerFactory.create_logger("Database")
+    file.logging(message)
+    console.logging(message)
+    database.logging(message)
 
-file.logging(message)
-console.logging(message)
-database.logging(message)
+if __name__ == "__main__":
+    main()
